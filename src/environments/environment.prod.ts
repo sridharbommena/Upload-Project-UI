9d5b7b12d6c736fe02b4c3a47fca8add341e326f
sridharbommena <smartboysridhar@gmail.com>
2022-07-26 02:10:25 +0530

    removed everyting

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  APP_NAME: "Uploadify",
-  APP_DESC: "Upload the files and get shareable link valid for 5 days",
-  AT_LEAST_ONE_UPLOAD_ERROR: "Please select at least one file to upload",
-  SIZE_EXCEEDE: "Size limit exceeded, Upload files less than 1.5GB"
-};
